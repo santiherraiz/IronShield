6b7d57c4d08da9cc93f491668107fdd9d9028ac2
Daniel <danpasmar9@alu.edu.gva.es>
2026-01-04 13:51:20 +0100

    Servidor - Cliente y Gestion Gestion de hilos, falta union con el puerto y la base de datos

--- src/main/java/org/DTO/Cliente.java
+++ src/main/java/org/DTO/Cliente.java
@@ -1,21 +1,47 @@
-import java.io.IOException;
+package org.DTO;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) {
-        String host = "";
+        // Datos de conexión (mismo puerto)
+        String host = "localhost";
         int puerto = 1234;
 
         try (Socket socket = new Socket(host, puerto)) {
-            System.out.println("Intentando conectar con IronShield...");
+            System.out.println("--- MÓVIL CONECTADO AL SISTEMA IRON SHIELD ---");
 
-            if (socket.isConnected()) {
-                System.out.println("Conexion acceptada");
+            // Comunicacion
+            PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            Scanner sc = new Scanner(System.in);
+            String mensajeUsuario;
+
+            // Escribe en la consola y se envía al servidor
+            System.out.println("Escribe coordenadas o mensaje (escribe 'salir' para terminar):");
+            while (true) {
+                System.out.print("> ");
+                mensajeUsuario = sc.nextLine();
+
+                if ("salir".equalsIgnoreCase(mensajeUsuario)) {
+                    break;
+                }
+
+                // 1. Enviar al servidor
+                salida.println(mensajeUsuario);
+
+                // 2. Esperar respuesta del servidor (ACK)
+                String respuesta = entrada.readLine();
+                System.out.println("SERVIDOR DICE: " + respuesta);
             }
 
-        } catch (IOException e) {
-            System.err.println("No se pudo conectar");
+        } catch (Exception e) {
+            System.err.println("Error en el cliente: " + e.getMessage());
         }
     }
 }
