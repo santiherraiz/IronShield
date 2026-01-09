d84e1775259baf738d505999ce00fcca3537f281
XGM12 <xg_mosaic@proton.me>
2026-01-09 13:59:14 +0100

    [SERVER-IMPL] Implementado el servidor HTTPSERVER para los LOGS

--- src/main/java/org/Server/Servidor.java
+++ src/main/java/org/Server/Servidor.java
@@ -1,9 +1,11 @@
 package org.Server;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.sun.net.httpserver.HttpServer;
 import org.DTO.ClienteConex;
 
 public class Servidor {
@@ -14,6 +16,15 @@ public class Servidor {
 
     public static void main(String[] args) {
         System.out.printf("[LOG] Servidor iniciado en %s:%d\n", HOST, PORT);
+        try {
+            final HttpServer httpServer = HttpServer.create(new InetSocketAddress(HOST, PORT == 80 ? 8080 : PORT + 1), 0);
+            httpServer.createContext("/log-error", new LogController());
+            httpServer.setExecutor(null);
+            httpServer.start();
+            System.out.println("[LOG] LogController escuchando en /log-error");
+        } catch (IOException e) {
+            System.err.println("[ERROR] No se ha podido crear el Servidor HTTP: " + e.getMessage());
+        }
 
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             while (true) {
