72248cd9e35760534168fc04b972214a61ffd403
Daniel0343 <danpasmar9@alu.edu.gva.es>
2026-01-14 13:21:17 +0100

    Fichero Conexion BBDD añadido

---
+++ src/main/java/org/BBDD/ConexionBD.java
@@ -0,0 +1,36 @@
+package org.BBDD;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConexionBD {
+
+    private String url;
+    private String user;
+    private String password;
+
+    public ConexionBD() {
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream("database.properties"));
+
+            url = props.getProperty("db.url");
+            user = props.getProperty("db.user");
+            password = props.getProperty("db.password");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection conectar() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
