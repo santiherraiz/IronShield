2300cd382ad8e51842f22973cee746b9525f7deb
XGM12 <xg_mosaic@proton.me>
2026-01-19 10:35:12 +0100

    [SERVER-IMPL] Introducido concurrencia (hilos) al servidor, es decir, cada cliente es un hilo. Acepta multicliente

--- src/main/java/org/Server/Servidor.java
+++ src/main/java/org/Server/Servidor.java
@@ -6,6 +6,11 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * La clase {@link Servidor} es la que se encarga de las conexiones TCP con los clientes. Este genera un hilo por cada
+ * conexión. El objetivo es recibir o enviar el JSON pertinente al cliente. Por ejemplo, queremos recibir un JSON con los
+ * datos de los agentes, recibiremos 1 JSON con los datos del agente. Nunca se enviarán más de 1 JSON.
+ */
 public class Servidor {
 
     private static final int PORT = UtilsServer.getServerPort("server.properties");
@@ -18,7 +23,8 @@ public class Servidor {
      * @return Devuelve el mensaje que ha leído.
      */
     private static String readClientMsg(Socket clientSocket) {
-        try (final var br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+        try {
+            final var br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             return br.readLine();
         } catch (Exception e) {
             UtilsServer.writeServerLog("[ERROR] No se ha podido leer el mensaje");
@@ -33,11 +39,16 @@ public class Servidor {
         try (final var serverSocket = new ServerSocket(PORT)) {
             while (true) {
                 final Socket clientSocket = serverSocket.accept();
-                final String json = readClientMsg(clientSocket);
-                System.out.println("[LOG]: " + json);
-                if (clientSocket.isClosed()) {
-                    UtilsServer.writeServerLog("[LOG] Cliente con IP " + clientSocket.getInetAddress().getHostAddress() + " se ha desconectado");
-                }
+                new Thread(() -> {
+                    final String json = readClientMsg(clientSocket);
+                    System.out.println("[LOG]: " + json);
+                    try {
+                        clientSocket.close();
+                        UtilsServer.writeServerLog("[LOG] Cliente desconectado: " + clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort());
+                    } catch (IOException e) {
+                        System.err.println("[ERROR] No se ha podido cerrar el socket del cliente.\n" + e.getMessage());
+                    }
+                }).start();
             }
         } catch (IOException e) {
             System.err.println("[ERROR] No se ha podido crear un Socket para el server.\n" + e.getMessage());
