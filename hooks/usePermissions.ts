bda81c516ed430bce26afbaecaa39b61876c38f0
manmarale <manmarale@alu.edu.gva.es>
2026-01-21 08:28:47 +0100

    feat: Permissions acabados, merge de rama

--- hooks/usePermissions.ts
+++
@@ -1,5 +0,0 @@
-interface PermissionState {
-    locationStatus: PermissionStatus;
-    requestLocationPermission: () => Promise<PermissionStatus>;
-    checkLocationPermission: () => Promise<PermissionStatus>;
-}
