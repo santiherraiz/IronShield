5708667053403abf1f0b6448afef0804ddb8e571
XGM12 <xg_mosaic@proton.me>
2026-01-23 12:13:54 +0100

    [FIX] Arreglado BBDD query

--- src/main/java/org/DAO/GuardiaDAO.java
+++ src/main/java/org/DAO/GuardiaDAO.java
@@ -6,13 +6,13 @@ import java.sql.PreparedStatement;
 
 public class GuardiaDAO {
 
-    public void insertarPosicion(int usuarioId, double latitud, double longitud) {
-        String sql = "INSERT INTO guardia_posicion (usuario_id, latitud, longitud) VALUES (?, ?, ?)";
+    public void insertarPosicion(String username, double latitud, double longitud) {
+        String sql = "INSERT INTO guardia_posicion (username, latitud, longitud) VALUES (?, ?, ?)";
 
         try (Connection conn = new ConexionBD().conectar();
              PreparedStatement ps = conn.prepareStatement(sql)) {
 
-            ps.setInt(1, usuarioId);
+            ps.setString(1, username);
             ps.setDouble(2, latitud);
             ps.setDouble(3, longitud);
 
