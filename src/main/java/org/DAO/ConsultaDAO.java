d8c06c2b2630c9cf9ed59a7e1278dbbc2f4d3ae1
Daniel <danpasmar9@alu.edu.gva.es>
2026-01-25 14:47:16 +0100

    [Consulta] Añadido insercción de nueva inactividad y lectura de las alertas

--- src/main/java/org/DAO/ConsultaDAO.java
+++ src/main/java/org/DAO/ConsultaDAO.java
@@ -1,38 +1,58 @@
 package org.DAO;
 
-
 import org.BBDD.ConexionBD;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConsultaDAO {
 
-    public void obtenerPosiciones(int usuarioId) {
+    public static List<String> obtenerAlertas() {
+
+        List<String> alertas = new ArrayList<>();
+
         String sql = """
-            SELECT latitud, longitud, fecha
-            FROM guardia_posicion
-            WHERE usuario_id = ?
-            ORDER BY fecha DESC
+            SELECT u.username, gp.latitud, gp.longitud, i.fecha
+            FROM inactividad i
+            JOIN usuarios u ON i.usuario_id = u.id
+            JOIN guardia_posicion gp ON gp.username = u.username
+            ORDER BY i.fecha DESC
         """;
+            //Si lo quieres ordenado por fecha vale, si no lo quitas
 
         try (Connection conn = new ConexionBD().conectar();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
-            ps.setInt(1, usuarioId);
-
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    System.out.println(
-                            rs.getDouble("latitud") + " , " +
-                                    rs.getDouble("longitud")
-                    );
-                }
+            while (rs.next()) {
+                String fila = rs.getString("username") + ";" +
+                        rs.getDouble("latitud") + ";" +
+                        rs.getDouble("longitud") + ";" +
+                        rs.getTimestamp("fecha");
+                alertas.add(fila);
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return alertas;
     }
+
+/*  Uso ejemplo pal xavi
+
+    List<String> alertas = InactividadDAO.obtenerAlertas();
+
+for (String alerta : alertas) {
+        String[] datos = alerta.split(";");
+        String username = datos[0];
+        double lat = Double.parseDouble(datos[1]);
+        double lon = Double.parseDouble(datos[2]);
+        String fecha = datos[3];
+*/
+
+
 }
