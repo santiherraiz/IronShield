09d68240de83ab1d48cf94c12dd94a6805211207
manmarale <manmarale@alu.edu.gva.es>
2026-02-04 10:14:43 +0100

    Fix: Cambiamo el pom para poder ejecutar jdk17

--- hooks/useServer.ts
+++ hooks/useServer.ts
@@ -3,6 +3,7 @@ export const useServer = () => {
     const SERVER_CASA = "http://192.168.1.38:45678" // Esto es para MI casa, cambiad a la vuestra si probáis: XAVI
     const SERVER_SANTI = "http://10.183.148.33:45678" // Esto es para SANTI
     const SERVER_SANTI2 = "http://192.168.1.43:45678" // Esto es para CASA SANTI
+    const SERVER_MANU = "http://10.249.54.134:45678" // Esto es para CASA SANTI
 
     /**
      * <strong>sendLocation</strong> envía la localización de la aplicación al servidor web. Primero comprobará si está en web o en
@@ -13,7 +14,7 @@ export const useServer = () => {
      */
     const sendLocation = async (username: string, latitude: string, longitude: string) => {
         try {
-            await fetch(`${SERVER_SANTI2}/location`, {
+            await fetch(`${SERVER_MANU}/location`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ code: 3, username, latitude, longitude }),
@@ -31,7 +32,7 @@ export const useServer = () => {
      */
     const getName = async (username: string, pass: string) => {
         try {
-            const res = await fetch(`${SERVER_SANTI2}/name`, {
+            const res = await fetch(`${SERVER_MANU}/name`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ code: 1, username, pass })
@@ -47,7 +48,7 @@ export const useServer = () => {
 
     const getActiveGuards = async () => {
         try {
-            const res = await fetch(`${SERVER_SANTI2}/guards`);
+            const res = await fetch(`${SERVER_MANU}/guards`);
             if (!res.ok) console.log("No se ha podido obtener la lista de guardias");
             return await res.json();
         } catch (error) {
